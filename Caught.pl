e47ba1d0bfdb0df72a50e5f09143c6ca37e82ce5
streetrees <streetrees.com@gmail.com>
2025-02-05 08:38:04 +0100

    newold

---
+++ Caught.pl
@@ -0,0 +1,23 @@
+Caught 
+
+I'm hiding in a woodpile when I'm caught, the woman takes my arm and leads me to a man with a chop saw blade,
+the other people who've been caught just wait in line,
+I caught the dragonfly floating on the surface, it was limp, and full of water.  
+I was completely caught off guard
+it's yellow and smudged and I caught it
+Maybe I simply caught you at a bad time,
+caught in a hurricane.
+caught in the February wind
+like I was caught in a trap and was wriggling to get out.
+caught a glimpse through the front window
+his car caught on fire within seconds
+
+
+when I can't catch my bus even though it's passed twice,
+turning it's branches and catching all the air,
+and nothing catches you surprised 
+catching on the windows and staying insects between the panes
+I really can't catch a break.
+it's time to check the catch,
+eventually catching him a few blocks down at a bus stop, where I cornered him,
+running down to the stream to catch the crane flies
---
+++ butterfly clips in my hair, falling out,.pl
@@ -0,0 +1,5 @@
+butterfly clips in my hair, falling out, I was never a fantasy,
+wanting to take your spoon to my lips and press the metal into me,
+our lips meet,
+snow holds the mountains and my lips hold my teeth
+I watch his lips form the words and I take them before he can speak
